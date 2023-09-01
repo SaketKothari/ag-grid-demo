cc4ec5ea661b5265e341c06254eb48a7e4b60a45
Saket Kothari <saketkothari722@gmail.com>
2023-09-01 10:02:15 +0530

    Loading data from server with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function App() {
-  const rowData = [
+  const [rowData, setRowData] = useState([
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
     { make: 'Porsche', model: 'Boxster', price: 72000 },
-  ];
+  ]);
 
-  const columnDefs = [
+  const [columnDefs, setColumnDefs] = useState([
     { field: 'make' },
     { field: 'model' },
     { field: 'price' },
-  ];
+  ]);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then((res) => res.json())
+      .then((rowData) => setRowData(rowData));
+  });
 
   return (
     <>
