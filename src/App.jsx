25ddc5ed7533f1a5d00be879653abcbe44120ee0
Saket Kothari <saketkothari722@gmail.com>
2023-09-01 10:43:30 +0530

    Create a Cell Renderer Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
+const SimpleComp = (params) => {
+  console.log(params);
+  return <>{params.value}</>;
+};
+
 function App() {
   const gridRef = useRef();
 
@@ -13,7 +18,7 @@ function App() {
   ]);
 
   const [columnDefs, setColumnDefs] = useState([
-    { field: 'make' },
+    { field: 'make', cellRenderer: SimpleComp },
     { field: 'model' },
     { field: 'price' },
   ]);
