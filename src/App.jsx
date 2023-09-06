79fa94a3d467d524f425da1e71134723a46501a5
Saket Kothari <saketkothari722@gmail.com>
2023-09-06 23:10:11 +0530

    Made tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,10 @@
-import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-
-import PriceFilter from './components/PriceFilter';
-import SimpleComp from './components/SimpleComp';
-import Rendering from './components/Rendering';
+import Tabs from './components/Tabs';
 
 function App() {
-  const gridRef = useRef();
-
-  const [rowData, setRowData] = useState([]);
-  const [columnDefs] = useState([
-    {
-      field: 'make',
-      cellRenderer: SimpleComp,
-    },
-    { field: 'model' },
-    {
-      field: 'price',
-      cellRenderer: Rendering,
-      filter: PriceFilter,
-      filterParams: {
-        title: 'My Custom Filter',
-      },
-    },
-  ]);
-
-  const defaultColDef = useMemo(
-    () => ({
-      flex: 1,
-      sortable: true,
-    }),
-    []
-  );
-
-  const cellClickedListener = useCallback((event) => {
-    console.log('cellClicked', event);
-  }, []);
-
-  useEffect(() => {
-    fetch('https://www.ag-grid.com/example-assets/row-data.json')
-      .then((res) => res.json())
-      .then((rowData) => setRowData(rowData));
-  }, []);
-
-  const buttonListener = useCallback(() => {
-    gridRef.current.api.deselectAll();
-  }, []);
-
   return (
-    <div className="app-container">
-      <button onClick={buttonListener}>Push Me</button>
-      <div
-        className="ag-theme-alpine"
-        style={{ width: '100%', height: '100vh' }}
-      >
-        <AgGridReact
-          ref={gridRef}
-          rowData={rowData}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          rowSelection="multiple"
-          animateRows={true}
-          onCellClicked={cellClickedListener}
-        />
-      </div>
-    </div>
+    <>
+      <Tabs />
+    </>
   );
 }
 
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Grid from './Grid';
+
+function Tabs() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabHeaders = [{ text: 'Code' }, { text: 'About' }, { text: 'Contact' }];
+
+  const tabContentData = [
+    {
+      title: 'Code',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique...Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique...',
+    },
+    {
+      title: 'About',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eum similique...',
+    },
+    {
+      title: 'Contact',
+      content: <Grid />,
+    },
+  ];
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="tabs">
+      <div className="tab-header">
+        {tabHeaders.map((header, index) => (
+          <div
+            key={index}
+            className={activeTab === index ? 'active' : ''}
+            onClick={() => handleTabClick(index)}
+          >
+            <i className={header.icon}></i> {header.text}
+          </div>
+        ))}
+      </div>
+      <div
+        className="tab-indicator"
+        style={{ top: `calc(80px + ${activeTab * 50}px)` }}
+      ></div>
+      <div className="tab-content">
+        {tabContentData.map((tab, index) => (
+          <div key={index} className={activeTab === index ? 'active' : ''}>
+            <h2>{tab.title}</h2>
+            <p>{tab.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tabs;
